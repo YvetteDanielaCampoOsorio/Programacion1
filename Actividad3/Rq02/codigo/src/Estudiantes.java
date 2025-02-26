88346947468bc72dee4f93b3ab01569c47c8adb2
yvette <danycomposo@gmail.com>
2025-02-25 23:37:40 -0500

    carpeta organisada, se agrego Rq 01,02 y 03

---
+++ Actividad3/Rq02/codigo/src/Estudiantes.java
@@ -0,0 +1,79 @@
+private class Estudiantes {
+
+    class Estudiantes {
+        private String id;
+        private String nombreCompleto;
+        private int edad;
+        private String genero;
+        private String alergias;
+        private String nombreAcudinte;
+        private String numeroContacto;
+
+        public Estudiantes(String id, String nombreCompleto,int edad, String genero, String alergias, String nombreAcudinte, String numeroContacto) {
+            this.id = id;
+            this.nombreCompleto = nombreCompleto;
+            this.edad = edad;
+            this.genero = genero;
+            this.alergias = alergias;
+            this.nombreAcudiente = nombreAcudiente;
+            this.numeroContacto = numeroContacto;
+        }
+
+        public String toString(){
+            return "nombre => " +nombre + "\n"
+
+        }
+
+        // Getters y setters
+        public String getId() {
+            return id;
+        }
+
+        public String getNombreCompleto() {
+            return nombreCompleto;
+        }
+
+        public void setNombreCompleto(String nombreCompleto) {
+            this.nombreCompleto = nombreCompleto;
+        }
+
+        public String getEdad() {
+            return edad;
+        }
+
+        public void setEdad(String edad) {
+            this.edad = edad;
+        }
+
+        public String getGenero() {
+            return genero;
+        }
+
+        public void setGenero(String genero) {
+            this.genero = genero;
+        }
+
+        public String getAlergias() {
+            return alergias;
+        }
+
+        public void setAlergias(String alergias) {
+            this.alergias = alergias;
+        }
+
+        public String getNombreAcudiente() {
+            return nombreAcudiente;
+        }
+
+        public void setnombreAcudiente(String nombreAcudiente) {
+            this.nombreAcudiente = nombreAcudiente;
+        }
+
+        public String getNumeroContacto() {
+            return numeroContacto;
+        }
+
+        public void setNumeroContacto(String numeroContacto) {
+            this.numeroContacto = numeroContacto;
+        }
+    }
